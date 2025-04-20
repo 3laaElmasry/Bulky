5099fd93e13a3b16ad7ba7935d5e4705cc571bf6
Elmasry <3laaelmasry2005a@gmail.com>
2025-04-21 00:34:36 +0200

    Add Client Side Validtion to Create Category View

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -6,12 +6,15 @@ namespace BulkyWeb.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage ="The Name Can't be blank or empty")]
+        [Required(ErrorMessage ="The Category Name Can't be blank or empty")]
         [StringLength(30,ErrorMessage = "The Category Name should be between 1 and 30")]
+        [Display(Name = "Category Name")]
+
         public string? Name { get; set; }
 
         [Range(minimum:1,maximum:100,
             ErrorMessage = "The Display Order should be 1-100")]
+        [Display(Name = "Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
