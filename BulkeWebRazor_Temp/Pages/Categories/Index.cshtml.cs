4e6936f04a37d4d00c538eba3e3651f6c3f06b34
Elmasry <3laaelmasry2005a@gmail.com>
2025-04-21 16:09:22 +0200

    Displary All Categories on Razor Page

--- BulkeWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkeWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,25 @@
+using BulkeWebRazor_Temp.Data;
+using BulkeWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.CompilerServices;
 
 namespace BulkeWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+
+        private readonly ApplicationDbContext _db;
+        public List<Category> Categories { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Categories = _db.categories.ToList();
         }
     }
 }
