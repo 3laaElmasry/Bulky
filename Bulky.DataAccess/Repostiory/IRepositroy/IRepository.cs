dacf45d3e7a0ff271aed19f87737c49b31f681a5
Elmasry <3laaelmasry2005a@gmail.com>
2025-04-22 15:40:49 +0200

    Implemnt Genraic Repository

--- Bulky.DataAccess/Repostiory/IRepositroy/IRepository.cs
+++ Bulky.DataAccess/Repostiory/IRepositroy/IRepository.cs
@@ -1,9 +1,26 @@
 ﻿
 
+using System.Linq.Expressions;
+
 namespace Bulky.DataAccess.Repostiory.IRepositroy
 {
     public interface IRepository<T> where T : class
     {
+        //T Category
+
+        IEnumerable<T> GetAll();
+
+        T Get(Expression<Func<T,bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+
+
 
     }
 }
