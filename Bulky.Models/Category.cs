60c5bbbb3e64bdbee7f52528b0b60d71195c4b07
Elmasry <3laaelmasry2005a@gmail.com>
2025-04-22 14:49:25 +0200

    N-Tier Archetecture

--- Bulky.Models/Category.cs
+++ Bulky.Models/Category.cs
@@ -1,18 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace BulkyWeb.Models
+namespace Bulky.Models
 {
     public class Category
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage ="The Category Name Can't be blank or empty")]
-        [StringLength(30,ErrorMessage = "The Category Name should be between 1 and 30")]
+        [Required(ErrorMessage = "The Category Name Can't be blank or empty")]
+        [StringLength(30, ErrorMessage = "The Category Name should be between 1 and 30")]
         [Display(Name = "Category Name")]
 
         public string? Name { get; set; }
 
-        [Range(minimum:1,maximum:100,
+        [Range(minimum: 1, maximum: 100,
             ErrorMessage = "The Display Order should be 1-100")]
         [Display(Name = "Display Order")]
         public int DisplayOrder { get; set; }
