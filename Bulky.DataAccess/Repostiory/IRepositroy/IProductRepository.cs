35823f3958bcd80cb54aefba66ce34a715f9bbf8
Elmasry <3laaelmasry2005a@gmail.com>
2025-04-23 10:40:05 +0200

    Create IProduct Repository & Product Repo

---
+++ Bulky.DataAccess/Repostiory/IRepositroy/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repostiory.IRepositroy
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+         void Update(Product obj);
+
+        void Save();
+    }
+}
---
+++ Bulky.DataAccess/Repostiory/ProductRepository.cs
@@ -0,0 +1,60 @@
+﻿
+
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repostiory.IRepositroy;
+using BulkyBook.Models;
+using System.Linq.Expressions;
+
+namespace BulkyBook.DataAccess.Repostiory
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(Product entity)
+        {
+            _db.Products.Add(entity);
+
+        }
+
+        public Product? Get(Expression<Func<Product, bool>> filter)
+        {
+            return _db.Products.FirstOrDefault(filter);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _db.Products.ToList();
+        }
+
+        public void Remove(Product entity)
+        {
+            _db.Remove(entity);
+
+        }
+
+        public void RemoveRange(IEnumerable<Product> entities)
+        {
+            _db.RemoveRange(entities);
+
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Update(obj);
+
+        }
+
+        
+    }
+}
