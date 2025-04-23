836ccd1ac538fd5a6805d10c719a2624ad8a2bbc
Elmasry <3laaelmasry2005a@gmail.com>
2025-04-23 21:18:30 +0200

    Update Product Custom Code

--- Bulky.DataAccess/Repostiory/ProductRepository.cs
+++ Bulky.DataAccess/Repostiory/ProductRepository.cs
@@ -47,10 +47,26 @@ namespace BulkyBook.DataAccess.Repostiory
 
         public void Update(Product obj)
         {
-            _db.Update(obj);
+            _db.Products.Update(obj);
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                if (obj.ImgUrl != null)
+                {
+                    objFromDb.ImgUrl = obj.ImgUrl;
+                }
 
+            }
         }
-
         
     }
 }
