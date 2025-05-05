ae9937bd70097401c6b0c3f3a0b0c49eb43b111d
Elmasry <3laaelmasry2005a@gmail.com>
2025-05-05 19:15:29 +0300

    Creaet View Component

---
+++ BulkyWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using BulkyBook.DataAccess.Repostiory.IRepositroy;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId is not null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) is null)
+                {
+                    IEnumerable<ShoppingCart> carts = await _unitOfWork.ShoppingCartRepo
+                        .GetAllAsync(c => c.ApplicationUserId == userId);
+                    HttpContext.Session.SetInt32(SD.SessionCart, carts.Count());
+                    return View(carts.Count());
+
+                }
+
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
